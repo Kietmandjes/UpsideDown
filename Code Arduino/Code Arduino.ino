bfce6c68b744d1e3cb3670a2c9c12025d1ac1e63
Iqgor <herrebrughigor@gmail.com>
2023-01-23 13:27:51 +0100

    arduino code versie 1.0

--- Code Arduino/Code Arduino.ino
+++ Code Arduino/Code Arduino.ino
@@ -1,20 +1,48 @@
+#define RED_LED 6
+#define GREEN_LED 5
+#define BLUE_LED 9
 int vib_pin=7;
-int led_pin=13;
+
+
 void setup() {
-  pinMode(vib_pin,INPUT);
-   pinMode(led_pin,OUTPUT);
+  pinMode(vib_pin, INPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
 }
 
 void loop() {
   int val;
-  val=digitalRead(vib_pin);
-   if(val==1)
-  {
-    digitalWrite(led_pin,HIGH);
+  val = digitalRead(vib_pin);
+  if (val == 1) {
     delay(1000);
-    digitalWrite(led_pin,LOW);
-     delay(1000);
-   }
-   else
-   digitalWrite(led_pin,LOW);
+    analogWrite(RED_LED, 255);
+    analogWrite(GREEN_LED, 255);
+    analogWrite(BLUE_LED, 255);
+    delay(50);
+    analogWrite(RED_LED, 0);
+    analogWrite(GREEN_LED, 0);
+    analogWrite(BLUE_LED, 0);
+    delay(50);
+    analogWrite(RED_LED, 255);
+    analogWrite(GREEN_LED, 255);
+    analogWrite(BLUE_LED, 255);
+    delay(50);
+    analogWrite(RED_LED, 0);
+    analogWrite(GREEN_LED, 0);
+    analogWrite(BLUE_LED, 0);
+    delay(50);
+    analogWrite(RED_LED, 255);
+    analogWrite(GREEN_LED, 255);
+    analogWrite(BLUE_LED, 255);
+    delay(3000);
+    analogWrite(RED_LED, 0);
+    analogWrite(GREEN_LED, 0);
+    analogWrite(BLUE_LED, 0);
+
+  } else
+    analogWrite(RED_LED, 0);
+    analogWrite(GREEN_LED, 0);
+    analogWrite(BLUE_LED, 0);
 }
+
